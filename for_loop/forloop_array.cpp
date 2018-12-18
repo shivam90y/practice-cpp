7d5bb5dc7354646a495ad1a2f5b46d95d704819c
Shivam Yadav <shivamy@coindelta.com>
2018-12-18 21:58:23 +0530

    for_loop

---
+++ for_loop/forloop_array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[8] = {2, 3, 4, 5, 8, 9};
+    for (int i = 0; i < 6; i++)
+        cout << arr[i] << endl;
+    return 0;
+}
---
+++ for_loop/infinite_for_loop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i = 1; i >= 1; i++)
+        cout << "This is infinite loop" << endl;
+    return 0;
+}
