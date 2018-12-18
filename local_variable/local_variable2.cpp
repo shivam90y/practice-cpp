fea9b3bf4a15e96ee709b3268466af4425f52def
Shivam Yadav <shivamy@coindelta.com>
2018-12-18 15:03:09 +0530

    local_variable

---
+++ local_variable/local_variable2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    float div;
+    a = 20;
+    b = 30;
+    sum = a + b;
+    cout << sum << endl;
+    div = (float)sum / a;
+    cout << div << endl;
+    return 0;
+}
